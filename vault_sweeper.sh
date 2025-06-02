d7c737e58e3e83b37b38c1a77eec40b0d49206a9
Dhruv Unde <dhruvunde@gmail.com>
2025-06-02 10:20:56 +0530

    update

--- vault_sweeper.sh
+++ vault_sweeper.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Hello world!"
+if [ -z "$1" ]; then
+    echo "Usage: $0 <directory_path>"
+    exit 1
+fi
+
+dir="$1"
+
+if [ -d "$dir" ]; then
+    find "$dir" -print
+else
+    echo "Invalid directory path: $dir"
+    exit 1
+fi
